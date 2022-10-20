9215000f8b26d746b90f1b06f6175b74a67c2eca
Malcolm McQueen <malcolmmcqueen@live.com>
2022-10-20 13:05:54 +0000

    create admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV["HTTP_USERNAME"], :password => ENV["HTTP_PASSWORD"]
   def show
+    @products = Product.all
+    @categories = Category.all
   end
 end
